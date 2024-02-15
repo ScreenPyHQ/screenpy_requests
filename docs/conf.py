472941319503fd6b91ff6eee7625e10b1fe68b99
Marcel Wilson <trenchrats@gmail.com>
2024-02-14 18:12:25 -0600

    sync conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,24 +9,27 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import sys
-
 
 sys.path.insert(0, os.path.abspath("./ext"))
 sys.path.insert(0, os.path.abspath("../"))
 
-from screenpy_requests.__version__ import __version__  # noqa: need the path first
+from screenpy_requests.__version__ import (
+    __version__,
+    __author__,
+    __copyright__,
+)  # noqa: need the path first
 
 autodoc_mock_imports = ["selenium", "screenpy", "screenpy_pyotp"]
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'screenpy_requests'
-copyright = '2022-2023, Perry Goy'
-author = 'Perry Goy'
+project = "screenpy_requests"
+copyright = __copyright__
+author = __author__
 
 # The full version, including alpha/beta/rc tags
 release = __version__
@@ -49,7 +52,6 @@ intersphinx_mapping = {
     "requests": ("https://docs.python-requests.org/en/latest/", None),
 }
 
-autodoc_member_order = "bysource"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -59,10 +61,20 @@ rst_prolog = """.. External references
 .. |Requests| replace:: `Requests <https://requests.readthedocs.io/en/master/>`__
 """
 
+
+
+
+
+
+
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -70,12 +82,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Other HTML settings
+autodoc_member_order = "bysource"
 add_module_names = False
